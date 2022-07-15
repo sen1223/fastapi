83a98513ad94ba84e9d414fc5988393e657b75c6
Usen <yqusenn@yahoo.co.jp>
2022-07-15 10:42:37 +0900

    change 20220715

--- venv/Scripts/deactivate.bat
+++ venv/Scripts/deactivate.bat
@@ -1,24 +1,5 @@
 @echo off
 
-<<<<<<< HEAD
-set VIRTUAL_ENV=
-
-REM Don't use () to avoid problems with them in %PATH%
-if not defined _OLD_VIRTUAL_PROMPT goto ENDIFVPROMPT
-    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
-    set _OLD_VIRTUAL_PROMPT=
-:ENDIFVPROMPT
-
-if not defined _OLD_VIRTUAL_PYTHONHOME goto ENDIFVHOME
-    set "PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%"
-    set _OLD_VIRTUAL_PYTHONHOME=
-:ENDIFVHOME
-
-if not defined _OLD_VIRTUAL_PATH goto ENDIFVPATH
-    set "PATH=%_OLD_VIRTUAL_PATH%"
-    set _OLD_VIRTUAL_PATH=
-:ENDIFVPATH
-=======
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
 )
@@ -39,4 +20,3 @@ set VIRTUAL_ENV=
 set VIRTUAL_ENV_PROMPT=
 
 :END
->>>>>>> origin/main
